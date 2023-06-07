43a11c64eeee8b30585dca0674f78f13a724bacc
aprados <andresprator@gmail.com>
2023-06-07 22:07:31 +0200

    (build) Docker image

---
+++ docker/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+if [ "$(which docker-compose)" ]; then
+	compose='docker-compose'
+else
+	compose='docker compose'
+fi
+
+echo $compose
+$compose up -d
+
+echo -e "\nWait for the UI to start then point your browser to: http://localhost:$(docker compose ps --status running --format json | sed -e 's/.*"PublishedPort":\([0-9]\+\),.*/\1/g') \n\n"
+echo "To stop showing the logs precc CTRL-C"
+$compose logs -f
