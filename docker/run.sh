d19d0f0e221c630a47554f2f04613d03ea5071b3
aprados <andresprator@gmail.com>
2023-06-08 12:58:43 +0200

    (fix) Docker compose run without build

--- docker/run.sh
+++ docker/run.sh
@@ -8,7 +8,8 @@ else
 fi
 
 echo $compose
-$compose up -d
+#$compose up -d
+docker compose up -d --no-build
 
 echo -e "\nWait for the UI to start then point your browser to: http://localhost:$(docker compose ps --status running --format json | sed -e 's/.*"PublishedPort":\([0-9]\+\),.*/\1/g') \n\n"
 echo "To stop showing the logs precc CTRL-C"
