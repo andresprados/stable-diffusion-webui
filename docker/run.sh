258e8abb115463d265a6ccff3203d4e91feac3cb
aprados <andresprator@gmail.com>
2023-08-15 21:32:33 +0200

    (build) Github actions automatic docker build and push

--- docker/run.sh
+++ docker/run.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-if [ "$(which docker-compose)" ]; then
-	compose='docker-compose'
-else
-	compose='docker compose'
-fi
-
-echo $compose
-#$compose up -d
 docker compose up -d --no-build
 
 echo -e "\nWait for the UI to start then point your browser to: http://localhost:$(docker compose ps --status running --format json | sed -e 's/.*"PublishedPort":\([0-9]\+\),.*/\1/g') \n\n"
